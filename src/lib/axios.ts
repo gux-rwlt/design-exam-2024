b272a4005065bb2d89959488c2a35bdb65fadb3d
Yuki Nakahori <nakahori@mail.glory.co.jp>
2025-01-09 19:22:15 +0900

    wip

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,6 @@
-import { API_URL } from "@/config";
 import Axios from "axios";
 
-const baseURL = API_URL;
+const baseURL = import.meta.env.VITE_API_URL;
 
 export const axios = Axios.create({
   baseURL: baseURL,
