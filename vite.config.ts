12313bbd43039e7f983dfa001a7a3ebf74ce6932
Yuki Nakahori <nakahori@mail.glory.co.jp>
2025-01-09 19:06:31 +0900

    sixth commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { resolve } from "path";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
-  },
-});
+// https://vitejs.dev/config/
+export default defineConfig(
+  ({ mode }
+  ) => {
+    const env = loadEnv(mode, process.cwd());
+
+    return {
+      base: `/${env.VITE_X_BASE_URL}`,
+      plugins: [react()],
+      resolve: {
+        alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
+      },
+    };
+  }
+)
